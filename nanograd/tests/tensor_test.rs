791db943240fa6f2ec5760c361b46ba1b8f2fc98
Tarek-Sallam <tarek.i.sallam@gmail.com>
2025-04-15 23:10:29 -0400

    fixed test bugs

--- nanograd/tests/tensor_test.rs
+++ nanograd/tests/tensor_test.rs
@@ -1,7 +1,8 @@
-use nanograd::Tensor;
+use nanograd::tensor::DataType;
+use nanograd::tensor::Tensor;
 
 fn create_tensor() {
     let data = vec![1, 2, 3, 4, 5];
-    let shape = vec![5]
-    t = Tensor.new(data, shape, Int64)
+    let shape = vec![5];
+    let _t = Tensor::new(data, shape, DataType::Int64);
 }
