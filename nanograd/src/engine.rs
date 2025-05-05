f851871b804421c36b86fd0929de2ea5d637c5dd
Tarek-Sallam <tarek.i.sallam@gmail.com>
2025-05-04 20:41:32 -0400

    created engine backward function for backprop

--- nanograd/src/engine.rs
+++ nanograd/src/engine.rs
@@ -1,1 +1,10 @@
+use crate::tensor::Tensor;
+use std::collections::HashMap;
 
+pub struct Engine;
+
+impl Engine {
+    pub fn backward<T>(output: &Tensor<T>, wrt: &Tensor<T>) -> Vec<T>
+    where:
+        T: Copy + Default + std::ops::Add<Output = T> + From<f32>
+}
--- nanograd/src/ops/grads.rs
+++ nanograd/src/ops/grads.rs
@@ -1,1 +1,15 @@
+use crate::ops::ops::{Op, OpType};
+use crate::tensor::Tensor;
+use std::collections::HashMap;
 
+pub trait Grad<T> {
+    fn grad(
+        &self,
+        output: &Tensor<T>,
+        grad_out: &[T],
+        grads: &mut HashMap<usize, Vec<T>>,
+        stack: &mut Vec<Tensor<T>>,
+    );
+}
+
+pub 
