bf4412f5a728a5d4fee689a5a0b948968c8d4321
Tarek-Sallam <tarek.i.sallam@gmail.com>
2025-05-10 20:43:45 -0400

    updated

--- nanograd/src/transformations/grad.rs
+++ nanograd/src/transformations/grad.rs
@@ -3,7 +3,5 @@ use crate::tensor::Tensor;
 
 pub fn grad(output: &Tensor, input: &Tensor, seed_grad: f32) -> Option<Tensor> {
     let grads = compute_gradients(output, seed_grad);
-    // In a real implementation, we would need to match the input tensor with its gradient
-    // For now, we'll just return the first gradient if available
     grads.into_iter().next()
 }
